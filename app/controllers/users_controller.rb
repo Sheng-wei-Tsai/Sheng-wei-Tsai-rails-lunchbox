2aed5ac10d05767f9d08d3ff0a3be82d61da0a1b
HenryTsai <henry88002605@gmail.com>
2020-04-01 14:44:24 +0800

    simplify controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,8 @@ class UsersController < ApplicationController
   end
 
   def registration
-    if user_params[:password] == user_params[:password_confirm]
       # 新增使用者 & 自動幫他登入
       @user = User.new(user_params)
-
       if @user.save 
       #成功
       # TODO:密碼加密 
@@ -20,16 +18,10 @@ class UsersController < ApplicationController
       #失敗
       #失敗通常不會轉址到原來頁面，這樣之前填的東西全部要再重填
         # 網誌並沒有變，借sign_up 的erb檔 留在原本網址的地方，只是畫面被重新渲染
-        render :sign_up 
-        
-        # redirect_to '/sign_up' 繞一圈主回來
-        
+        render :sign_up      
+        # redirect_to '/sign_up' 繞一圈主回來 
       end
       # (email: user[:password_confirm], password: user[:password])
-      
-    else 
-      redirect_to '/sign_up'
-    end
     # render html: params[:user]
   end
 
@@ -38,7 +30,7 @@ class UsersController < ApplicationController
     # strong parameters (安全性考量)
     params.require(:user).permit( :email, 
                                   :password, 
-                                  :password_confirm)
+                                  :password_confirmation)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
   # 虛擬欄位(可讀可寫的方法) 本生沒有 password_confirm 欄位
-  attr_accessor :password_confirm
-
   validates :email, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, confirmation: true
 end
