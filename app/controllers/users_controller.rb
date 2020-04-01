c7a8017d6f65952ed04adde27e4876b5a97315fc
HenryTsai <henry88002605@gmail.com>
2020-04-01 13:36:19 +0800

    add registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,21 @@ class UsersController < ApplicationController
   def registration
     user = params[:user]
     if user[:password] == user[:password_confirm]
-      redirect_to '/login'
+      # 新增使用者 & 自動幫他登入
+      
+      # strong parameters (安全性考量)
+      clean_user = params.require(:user).permit(:email, :password, :password_confirm)
+      u = User.new(clean_user)
+      
+      if u.save 
+      #成功
+        redirect_to '/' 
+      else
+      #失敗
+        redirect_to '/sign_up'
+      end
+      # (email: user[:password_confirm], password: user[:password])
+      
     else 
       redirect_to '/sign_up'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  # 虛擬欄位
+  attr_accessor :password_confirm
+
+  validates :email, presence: true
+  validates :password, presence: true
 end
