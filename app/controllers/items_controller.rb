11496946a3bdbba2694d55136725332f457f1a13
HenryTsai <henry88002605@gmail.com>
2020-04-03 11:06:04 +0800

    add views

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,30 @@
+class ItemsController < ApplicationController
+  def index
+  end
+
+  def new
+    @item = Item.new  
+  end
+
+  def create
+    @item = Item.new(item_params)
+    
+    if  @item.save
+      # flash[:notice] = "成功新增餐點"
+      # redirect_to items_path
+      redirect_to items_path , notice: '成功新增餐點'
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit( :name,
+                                  :description,
+                                  :price,
+                                  :spec )
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,27 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+
+  resources :items # 8條路徑
+
+
+
+
+
+
+
+
+
+  # user
   # get "/", to: "welcome#index"
   # 泛用型的寫法
   root "welcome#index"
-
   get '/login', to: "users#login"
   post '/login', to: 'users#sign_in'
   delete "/logout", to: "users#logout"
-
   get '/sign_up', to: "users#sign_up"
   post '/sign_up', to: "users#registration"
-  
-  
   # get post put patch delete (現在瀏覽器只有get/post 這兩個動作)
-
   # 方法   controller 見一個檔案
   
 end
