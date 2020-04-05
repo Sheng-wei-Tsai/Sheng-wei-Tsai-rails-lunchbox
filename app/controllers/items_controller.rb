c60ca0e2d0f8ad846bccda693f92c474377c4205
HenryTsai <henry88002605@gmail.com>
2020-04-05 16:53:53 +0800

    bundle install

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,14 @@
 class ItemsController < ApplicationController
+
+rescue_from ActiveRecord::RecordNotFound, 
+            with: :active_record_not_found 
+
   def index
+    @items = Item.all # select * from ..
+  end
+
+  def show
+    @item = Item.find(params[:id]) 
   end
 
   def new
@@ -18,12 +27,24 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to items_path, notice: 'deleted successfully'
+  end
+
   private
   def item_params
     params.require(:item).permit( :name,
                                   :description,
                                   :price,
                                   :spec )
+  end
+
+  def active_record_not_found
+    render  file: 'public/404.html', 
+            status: 404,
+            layout: false
   end
 
 
