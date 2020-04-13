555dabd3f96c3e19a3efc21af0234ce43e334c14
HenryTsai <henry88002605@gmail.com>
2020-04-13 16:45:22 +0800

    cart initialize

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,21 +1,28 @@
 require 'rails_helper'
 
-
-
 RSpec.describe Cart, type: :model do
   describe "基本功能" do
     it "可以把商品丟到購物車中，然後購物車裡就有東西了" do
-
       # Arrange
       cart = Cart.new
-
       # Act
       cart.add_item(1)
-
       # Assert
-      expect(cart.empty?).to be false
-    
+      expect(cart.empty?).to be false  
     end
+
+    it "加了相同種類的商品到購物車裡，購買項目（CartItem）並不會增加，但商品的數量會改變" do
+      cart = Cart.new 
+
+      cart.add_item(1)
+      cart.add_item(1)
+      cart.add_item(2)
+      cart.add_item(2)
+      cart.add_item(1)
+
+      expect(cart.items.count).to be 2
+    end
+
   end
 
   describe "進階功能" do
