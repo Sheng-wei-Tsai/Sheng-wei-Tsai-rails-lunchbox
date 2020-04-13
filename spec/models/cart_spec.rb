b59128344f5a3f82a2829ddf6ea260734af991d4
HenryTsai <henry88002605@gmail.com>
2020-04-13 16:24:58 +0800

    move models cart_spec

---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+
+
+RSpec.describe Cart, type: :model do
+  describe "基本功能" do
+    it "可以把商品丟到購物車中，然後購物車裡就有東西了" do
+
+      # Arrange
+      cart = Cart.new
+
+      # Act
+      cart.add_item(1)
+
+      # Assert
+      expect(cart.empty?).to be false
+    
+    end
+  end
+
+  describe "進階功能" do
+  end
+end
+
