d9b8121c768b926dd16ecf7312d95784b5aa1f37
HenryTsai <henry88002605@gmail.com>
2020-04-14 11:16:47 +0800

    基本功能2

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,9 +3,20 @@ class Cart
   def initialize
     @items = []
   end 
-  
+
   def add_item(item_id)
-    @items << item_id
+    # 檢查 陣列裡面有沒有我們要的 cart_item
+    found_item = @items.find {|item| item.item_id == item_id }
+    #                            cart_item(上的id)  
+    # found.class => CartItem 你是誰生出來的
+    if found_item
+      # 增加數量
+      found_item.increment! # 增加數量
+    else
+      # 找不到 創造一個給他
+      @items << CartItem.new(item_id, 1)
+    end
+
   end
 
   def empty?
---
+++ app/models/cart_item.rb
@@ -0,0 +1,17 @@
+# CartItem.new(3, 1)
+# CartItem.new(3)
+# 多考慮預設值
+
+class CartItem
+  attr_reader :item_id, :quantity
+  #在外部希望可以讀取 
+
+  def initialize(item_id, quantity = 1)
+    @item_id = item_id
+    @quantity = quantity
+  end
+
+  def increment!(amount = 1 )
+    @quantity += amount 
+  end
+end
