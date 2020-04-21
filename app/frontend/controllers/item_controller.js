0dce9edc42925b56d39458686a5446b8152e12f1
HenryTsai <henry88002605@gmail.com>
2020-04-21 15:46:46 +0800

    add rails ajax

--- app/frontend/controllers/item_controller.js
+++ app/frontend/controllers/item_controller.js
@@ -8,42 +8,62 @@
 // </div>
 
 import { Controller } from "stimulus"
-import axios from "axios"
+// import axios from "axios"
+import Rails from "@rails/ujs"
 
 export default class extends Controller {
   static targets = ["icon"]
 
-  initialize() {
-    // @clicked = false ruby
-    this.clicked = false;
-  }
-
   heart(e) {
     e.preventDefault();
-    
-    const csrfToken = document.querySelector('[name=csrf-token]').content
-    axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
 
     let item_id = document.querySelector('#item_id').value;
 
-    axios.post(`/api/v1/items/${item_id}/favorite`)
-    // 這邊要寫arrow function 因為是不同的 scope
-    .then(resp => {
-      if(resp.data.status === "favorited") {
-          console.log('Yes')
-         this.iconTarget.classList.remove('far'); // s = solid
-         this.iconTarget.classList.add('fas');    // r = regular
-      } else {
-        console.log('No')
+    Rails.ajax({
+      url: `/api/v1/items/${item_id}/favorite`, 
+      type: 'POST', 
+      success: resp => {
+        if (resp.status === "favorited") {
+          this.iconTarget.classList.remove('far');
+          this.iconTarget.classList.add('fas');
+        } else {
           this.iconTarget.classList.remove('fas');
           this.iconTarget.classList.add('far');
-      }
-    })
-    .catch( err => {
-      console.log(err);
+        }
+      }, 
+      error: err => {
+        console.log(err);
+      } 
     })
   }
 }
+
+//   heart(e) {
+//     e.preventDefault();
+//     // axios
+//     // const csrfToken = document.querySelector('[name=csrf-token]').content
+//     // axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+
+//     let item_id = document.querySelector('#item_id').value;
+
+//     axios.post(`/api/v1/items/${item_id}/favorite`)
+//     // 這邊要寫arrow function 因為是不同的 scope
+//     .then(resp => {
+//       if(resp.data.status === "favorited") {
+//           console.log('Yes')
+//          this.iconTarget.classList.remove('far'); // s = solid
+//          this.iconTarget.classList.add('fas');    // r = regular
+//       } else {
+//         console.log('No')
+//           this.iconTarget.classList.remove('fas');
+//           this.iconTarget.classList.add('far');
+//       }
+//     })
+//     .catch( err => {
+//       console.log(err);
+//     })
+//   }
+// }
     // console.log('Heart is clicked');
   //   if (this.clicked) {
   //     this.iconTarget.classList.remove('far'); // s = solid
