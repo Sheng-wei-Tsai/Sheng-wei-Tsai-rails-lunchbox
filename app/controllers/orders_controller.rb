ffdb24f24c4328e8ab288b57fa485ba47af1d0cf
HenryTsai <henry88002605@gmail.com>
2020-04-27 14:25:57 +0800

    新增訂單日期+亂碼

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,26 +4,32 @@ class OrdersController < ApplicationController
     # @order = Order.new(order_params)
     # @order.user = current_user = 下面那行
     @order = current_user.orders.build(order_params) # 以使用者的角度來看
-    if @order.save
 
-    else
+    current_cart.items.each do |cart_item|
+      item = OrderItem.new( item: cart_item.item,
+                            price: cart_item.item.price,
+                            quantity: cart_item.quantity)
+      @order.order_items << item
+      end
 
-    end
-
-
-
-
-
-
-
-    render html: params
+      if @order.save
+        # 刷卡
+        # 清空購物車
+        redirect_to root_path, notice: 'ok'
+      else
+        redirect_to root_path, notice: 'not ok'
+      end
+  
+    
   end
 
-
-
   private
-
   def order_params
     params.require(:order).permit(:receiver, :phone, :address)
   end
+
 end
+
+
+  
+
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,27 @@
 class Order < ApplicationRecord
   has_many :order_items
   belongs_to :user
+
+  before_create :create_order_num
+  
+  private
+
+  def create_order_num
+    self.order_num = num_generator
+  end
+
+  def num_generator
+    #實作內容
+    # p Date.new(today)
+    year = Date.today.year
+    month = Date.today.month
+    day = Date.today.day
+  
+     code = [*0..9, *'A'..'Z'].sample(6).join
+  
+  # ex: 20200427A14WEF
+  "#{year}#{"%02d" % month}#{"%02d" % day }#{code}"
+    
+  end
+
 end
